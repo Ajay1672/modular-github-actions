4678a9f6e5c50ad8b4021315a08f10a245838fb6
Ajay1672 <mauryaajay9594@gmail.com>
2025-01-07 12:47:25 +0530

    fvjjv

--- modules/S3/main.tf
+++ modules/S3/main.tf
@@ -2,17 +2,6 @@ resource "aws_s3_bucket" "aws_s3" {
    
    bucket = "var.bucket-${random_string.unique_suffix.result}"
    acl = "private"
-
-
-   lifecycle_rule {
-    enabled = true
-
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA"
-    }
-
-  }
 }
 
 resource "random_string" "unique_suffix" {
